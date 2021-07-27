fab9d61eb46645546bf3ff87bbfd5c5b44852dd9
Denis <den.zalman76@gmail.com>
2021-07-28 00:31:46 +0300

    remove bash outputs

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,19 +4,22 @@ set -e
 ######
 
 echo "AWS configuration..."
-aws configure set default.region "${INPUT_LAMBDA_REGION}"
+aws configure set default.region "${INPUT_LAMBDA_REGION}" > /dev/null 2>&1
+echo "OK"
 
 ######
 
 echo "Installing dependencies..."
 mkdir dependencies
-pip install --target=python -r "${INPUT_REQUIREMENTS_TXT}"
+pip install --target=python -r "${INPUT_REQUIREMENTS_TXT}" > /dev/null 2>&1
+echo "OK"
 
 ######
 
 echo "Zipping dependencies..."
 zip -r python.zip ./python
 rm -rf python
+echo "OK"
 
 ######
 
@@ -24,17 +27,20 @@ echo "Publishing dependencies layer..."
 response=$(aws lambda publish-layer-version --compatible-runtimes python3.8 --layer-name "${INPUT_LAMBDA_LAYER_ARN}" --zip-file fileb://python.zip)
 VERSION=$(echo $response | jq '.Version')
 rm python.zip
+echo "OK"
 
 ######
 
 echo "Deploying lambda main code..."
 zip -r lambda.zip . -x \*.git\*
-aws lambda update-function-code --function-name "${INPUT_LAMBDA_FUNCTION_NAME}" --zip-file fileb://lambda.zip
+aws lambda update-function-code --function-name "${INPUT_LAMBDA_FUNCTION_NAME}" --zip-file fileb://lambda.zip > /dev/null 2>&1
+echo "OK"
 
 ######
 
 echo "Updating lambda layer version..."
-aws lambda update-function-configuration --function-name "${INPUT_LAMBDA_FUNCTION_NAME}" --layers "${INPUT_LAMBDA_LAYER_ARN}:${VERSION}"
+aws lambda update-function-configuration --function-name "${INPUT_LAMBDA_FUNCTION_NAME}" --layers "${INPUT_LAMBDA_LAYER_ARN}:${VERSION}" > /dev/null 2>&1
+echo "OK"
 
 ######
 
